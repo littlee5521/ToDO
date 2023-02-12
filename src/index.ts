3cff7d362df395e977e4cd9ae440d8a3606288b9
Martin Rodriguez <68254461+littlee5521@users.noreply.github.com>
2023-02-12 12:40:42 -0800

    Note layout finished svg imported

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 const css1 = require('./css/reset.css');
 const css = require('./css/style.css');
+const book = require('./img/book.svg');
 import {newNote} from './script/notes'
 import {domutil} from './script/notes'
 const utils = new domutil
